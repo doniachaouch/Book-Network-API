d0de1cb921596b00e2b097462d0756be863c66e9
doniachaouch <donia.chaouch@maps-it.com>
2025-09-18 19:05:27 +0100

    Implement the authentification Provider

--- src/main/java/com/doniachaouch/book/security/SecurityConfig.java
+++ src/main/java/com/doniachaouch/book/security/SecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
@@ -51,6 +52,8 @@ public class SecurityConfig {
                         )
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authenticationProvider(authenticationProvider)
-                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
     }
 }
